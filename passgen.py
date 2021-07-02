4f6e38b1bdcffea8376932a1ca25a67d5a1ee4a8
Ori <45724082+therealOri@users.noreply.github.com>
2021-07-02 11:03:27 -0600

    Code Rework
    - Using secrets instead of random
    - Imported from `string import ascii_lowercase, ascii_uppercase, digits`, instead of writing them all out.
    - Changed the way the variables look/are orientated for the True block. 
    - Declaring "int" at the input variables on lines 55 & 56 to be better used later on.
    - Removed `random.sample` and fixed the print line to not have a formatted string.

--- passgen.py
+++ passgen.py
@@ -1,10 +1,10 @@
-import random
-import sys
+import secrets
+from string import ascii_lowercase, ascii_uppercase, digits 
 
-uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowercase_letters = uppercase_letters.lower() #uppercase_letters.lower() lowercases everything in uppercase_letters and uses that instead of just writing out another list.
-symbols = "!@#$%^&*()[]{},.;:-_/\\+?*|`~"
-digits = "0123456789"
+uppercase_letters = ascii_uppercase
+lowercase_letters = ascii_lowercase
+symbols = "!=<>'@#$%^&*()[]{},.;:-_/\\+?*|`~"
+numbers = digits
 korean = "ㅂㅋㅎㅭㅱㅶㅹㅺㅿㆁㆄㆅ"
 russian = "БГДЁЖИЙЛПФфЦЧШЩЪЫЬЭЮЯ"
 #Example |  new_list = "WHATERVER YOU WANT HERE"  | This can be named whatever you can think of, doesn't have to be "new_list". It's just what I am using for this example.
@@ -12,7 +12,15 @@ russian = "БГДЁЖИЙЛПФфЦЧШЩЪЫЬЭЮЯ"
 
 
 #Here we would add "new" to the line below with one more True. | the word doesn't have to be "new", it can be anything you want. It's just what I am using for this example.
-upper, lower, nums, syms, kor, rus = True, True, True, True, True, True #All are true so everything will be used in genrating your password. Setting one or more to False will exculde the False one(s) fron the generation.
+#All are true so everything will be used in genrating your password. Setting one or more to False will exculde the False one(s) fron the generation.
+upper = True
+lower = True
+nums = True
+syms = True
+kor = True
+rus = True
+
+
 
 all = ""
 
@@ -21,7 +29,7 @@ if upper:
 if lower:
     all += lowercase_letters
 if nums:
-    all += digits
+    all += numbers
 if syms:
     all += symbols
 if kor:
@@ -33,19 +41,25 @@ if rus:
 #The above here is an example of what you would do if you wanted to add more.
 
 
+
+
+
 print('Note: Your password(s) will be saved to pass.txt.\nPlease make sure to write it/them down or hash/encrypt the password(s) into a new text file before running this script again. \n')
 print('\n')
 
-with open('pass.txt', 'w'): pass #Clears txt file contents before writing into it with new passwords..or password.
+with open('pass.txt', 'w') as p:
+    p.close() #Clears txt file contents before apending/writing into it with new passwords. 
+              #Still working on how to do this during the code below, So I can just open pass.txt once rather than twice.
 
-length = input('How long do you want your password(s)?: ') #How long you want the password to be. (Longer and more complex, the better) You may need to change this depending on the limits some websites have for how long a password should be.
-amount = input('How many do you want generated?: ') #How many passwords you want generated.
+
+length = int(input('How long do you want your password(s)?: ')) #How long you want the password to be. (Longer and more complex, the better) You may need to change this depending on the limits some websites have for how long a password should be.
+amount = int(input('How many do you want generated?: ')) #How many passwords you want generated.
 
 print('\n')
 
-for x in range(int(amount)):
-    password = "".join(random.sample(all, int(length))) #"int" was added/specified for it to know the variables are numbers/integers. And won't work if you type/use strings when given the option to type.
+for x in range(amount):
+    password = ''.join(secrets.choice(all) for i in range(length))
     with open('pass.txt', 'a') as f:
-        print(f'{password}', file=f)
+        print(password, file=f)
+        f.close()
     print(f"Here is your newly generated random password: {password}")
-
